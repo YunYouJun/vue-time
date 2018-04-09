8986b17ce8e240dc0b35eb4ef8c0e2bb7a2db7e0
YunYouJun <me@yunyoujun.cn>
2018-04-09 16:19:44 +0800

    basic function: display current time

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # vue-time
-Display current time.
+
+> Display current time.
+
+```html
+<vue-time></vue-time>
+```
+
+Display: `2018年4月9日 星期一 16:15:24`
+
+参数 | 说明 | 类型 | 可选值 | 默认值
+---|---
+showDate | 显示年份日期 | boolean | —— | true
+showDay | 显示星期几 | boolean | —— | true
+showTime | 显示时间 | boolean | —— | true
---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <span v-if="showDate" class="vue-time-date">{{ now.year + '年' + now.month + '月' + now.date + '日' }}</span>
+    <span class="vue-time-weekday">{{ '星期' + weekday[now.day] }}</span>
+    <span class="vue-time-time">{{ now.hour + ':'+ now.minute + ':' + now.second }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vue-time',
+  data () {
+    return {
+      weekday: ['日','一','二','三','四','五','六'],
+      now: {
+        year: '',
+        month: '',
+        date: '',
+        day: '',
+        hour: '',
+        minute: '',
+        second: ''
+      }      
+    }
+  },
+  props: {
+    showDate: {
+      type: Boolean,
+      default: true
+    },
+    showDay: {
+      type: Boolean,
+      default: true
+    },
+    showTime: {
+      type: Boolean,
+      default: true
+    }
+  },
+  mounted () {
+    let _this = this
+    setInterval(_this.showNow, 1000)
+  },
+  methods: {
+    showNow() {
+      let now = new Date()
+      this.now.year = now.getFullYear()
+      this.now.month = now.getMonth() + 1
+      this.now.date = now.getDate()
+      this.now.day = now.getDay()
+      this.now.hour = this.addZero(now.getHours())
+      this.now.minute = this.addZero(now.getMinutes())
+      this.now.second = this.addZero(now.getSeconds())
+    },
+    addZero(num) {
+      if (num < 10) {
+        return '0' + num
+      }
+      return num
+    }
+  }
+}
+</script>
